b544f93be7dcc43ef3a273d1e6fd1775e3399b92
RenatoMoratto <renatowmoratto@gmail.com>
2021-10-20 14:11:34 -0300

    feat: Connect Client to DB

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,22 +13,14 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		Cliente cliente2 = new Cliente();
-		
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("43 99999-2222");
-		cliente1.setEmail("joaodasilva@algaworks.com");
-		
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("11 97777-1111");
-		cliente2.setEmail("mariadascouves@algaworks.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
+
 	}
 
 }
